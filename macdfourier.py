37aa8cf89082f0f682795503ad7fac81083d0b0e
syeh3141 <49661930+syeh3141@users.noreply.github.com>
2019-04-15 19:50:13 -0400

    Add files via upload

---
+++ macdfourier.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 29 13:20:17 2018
+
+@author: syeh3
+"""
+import macd
+import numpy as np
+from scipy.fftpack import fft
+import copy
+import matplotlib.pyplot as plt
+from collections import Counter
+
+def macdFourier(stockData):
+    """Takes in stockData in pandas.dataFrame type and returns index "cycle"
+    of maximum frequency bin after performed Discrete Fourier Transform"""
+    
+    macd.macd(stockData)
+    
+    macdsignal = []
+    
+    for signalDifference in stockData['MACD Signal Difference']:
+        
+        macdsignal.append(signalDifference)
+        
+    fourierd = fft(np.array(macdsignal))
+    
+    norm = np.absolute(fourierd)
+    
+    return norm.argmax()
+
+
+def macdFourierHalf(stockData):
+    
+    firstPoint = 0
+    
+    halfwayPoint = int(len(stockData)/2)
+    
+    allFrequencyBins = []
+    
+    while(firstPoint < int(len(stockData)/2)):
+        
+        partitionedStockData = copy.deepcopy(stockData[firstPoint:halfwayPoint])
+        
+        allFrequencyBins.append(macdFourier(partitionedStockData))
+        
+        firstPoint += 1
+        
+        halfwayPoint += 1
+        
+    frequencyBins = Counter(allFrequencyBins)
+    
+    return int(len(stockData)/2)/frequencyBins.most_common(1)[0][0]
+        
+    #return allFrequencyBins
+
+
+def macdFourierTwoThird(stockData):
+    
+    firstPoint = 0
+    
+    twoThirdPoint = int(2*len(stockData)/3)
+    
+    allFrequencyBins = []
+    
+    while(firstPoint < int(len(stockData)/3)):
+        
+        partitionedStockData = copy.deepcopy(stockData[firstPoint:twoThirdPoint])
+        
+        allFrequencyBins.append(macdFourier(partitionedStockData))
+        
+        firstPoint += 1
+        
+        twoThirdPoint += 1
+        
+    frequencyBins = Counter(allFrequencyBins)
+    
+    return int(2*len(stockData)/3)/frequencyBins.most_common(1)[0][0]
+
+
+def macdFourierOneThird(stockData):
+    
+    firstPoint = 0
+    
+    oneThirdPoint = int(len(stockData)/3)
+    
+    allFrequencyBins = []
+    
+    while(firstPoint < int(2*len(stockData)/3)):
+        
+        partitionedStockData = copy.deepcopy(stockData[firstPoint:oneThirdPoint])
+        
+        allFrequencyBins.append(macdFourier(partitionedStockData))
+        
+        firstPoint += 1
+        
+        oneThirdPoint += 1
+        
+    frequencyBins = Counter(allFrequencyBins)
+    
+    return int(len(stockData)/3)/frequencyBins.most_common(1)[0][0]
+
+
+def showHistogramPlot(listOfFrequencyBins):
+    
+    allFreqArray = np.array(listOfFrequencyBins)
+    
+    plt.hist(allFreqArray, bins = np.arange(allFreqArray.min(), allFreqArray.max() + 1))
+    
+    plt.show()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ macdfourierscript.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  4 17:20:41 2018
+
+@author: syeh3
+"""
+
+import macdfourier as mf
+import convertExceltoDataFrame
+import copy
+
+convertExceltoDataFrame.convertExcelToDataFrame('presentstockdata')
+
+stocksDataListOutcome = copy.deepcopy(convertExceltoDataFrame.stocksDataList)
+
+goodstockBins = {}
+
+for stock in stocksDataListOutcome:
+    
+    print(stock)
+    
+    stockData = stocksDataListOutcome[stock]
+    
+    oneThird = int(len(stockData)/3)
+    
+    oneHalf = int(len(stockData)/2)
+    
+    twoThird = int(2*len(stockData)/3)
+    
+    listBins = ([oneThird/mf.macdFourierOneThird(stockData), oneHalf/mf.macdFourierHalf(stockData), twoThird/mf.macdFourierTwoThird(stockData)])
+    
+    sortedListBins = sorted(listBins)
+    
+    print(sortedListBins)
+    
+    low = sortedListBins[0]
+    
+    mid = sortedListBins[1]
+    
+    high = sortedListBins[2]
+    
+    if (low >= (int)(0.85*mid) and high <= ((int)(1.15*mid) + 1)) or low == mid or mid == high:
+        
+        goodstockBins[stock] = listBins
+    
+print(goodstockBins)
+    
+    
+    
+    
